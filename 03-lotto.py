05108752640dee342c99178f70a13d35a22f999f
jjinmandu <115483146+jjinmandu@users.noreply.github.com>
2023-06-21 17:22:47 +0900

    Update 03-lotto.py

--- 03-lotto.py
+++ 03-lotto.py
@@ -25,3 +25,4 @@ if button:
     for i in range(1, 6):
         st.subheader(f'{i}. 행운의 번호: :green[{generate_lotto()}]')
     st.write(f"생성된 시각: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}")
+print('1234')
