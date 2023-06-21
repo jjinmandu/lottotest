04bdc54186d45f0a9fb277d22e2739a77e2ac4a7
jjinmandu <115483146+jjinmandu@users.noreply.github.com>
2023-06-21 17:24:50 +0900

    Update 03-lotto.py

--- 03-lotto.py
+++ 03-lotto.py
@@ -25,4 +25,26 @@ if button:
     for i in range(1, 6):
         st.subheader(f'{i}. 행운의 번호: :green[{generate_lotto()}]')
     st.write(f"생성된 시각: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}")
-print('1234')
+
+print("Content-Type: text/html")
+print()
+print('''<!doctype html>
+<html>
+<head>
+  <title>About Python</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.html">Python</a></h1>
+  <ol>
+    <li><a href="html.html">HTML</a></li>
+    <li><a href="css.html">CSS</a></li>
+    <li><a href="javascript.html">JavaScript</a></li>
+    <li><a href="python.html">Python</a></li>
+  </ol>
+  <h2>Python</h2>
+  <p>Python is a programming language that lets you work more quickly and integrate your systems more effectively.
+You can learn to use Python and see almost immediate gains in productivity and lower maintenance costs.</p>
+</body>
+</html>
+''')
